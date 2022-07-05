abd967e47e57601d419c9eb56a0de0ed3eaade8f
VelmaAkoch <velmaakochi1@gmail.com>
2022-07-05 10:47:39 +0300

    add splash screen activity

--- app/src/main/java/com/moringaschool/medicare/MainActivity.java
+++ app/src/main/java/com/moringaschool/medicare/MainActivity.java
@@ -2,13 +2,29 @@ package com.moringaschool.medicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.moringaschool.medicare.ui.activities.SplashScreenActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.txt);
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SplashScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
